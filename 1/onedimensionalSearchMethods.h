394ec0b30a194aeed98fb58ce51352e73ac7fd73
alecsmirnov <alecsmirnov93@gmail.com>
2019-06-22 23:54:26 +0700

    Initial commit

---
+++ 1/onedimensionalSearchMethods.h
@@ -0,0 +1,26 @@
+﻿#ifndef ONEDIMENSIONALSEARCHMETHODS_H
+#define ONEDIMENSIONALSEARCHMETHODS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef struct MethodResult {
+	double func_min;
+
+	uint32_t iters_count;
+	uint32_t calcs_count;
+} MethodResult;
+
+typedef struct IntervalResult {
+	double a;
+	double b;
+} IntervalResult;
+
+MethodResult dichotomyMethod(double (*func)(double), double a, double b, double eps, const char* filename);
+MethodResult goldenRatioMethod(double (*func)(double), double a, double b, double eps, const char* filename);
+MethodResult fibonacciMethod(double (*func)(double), double a, double b, double eps, const char* filename);
+
+IntervalResult findIntervalMin(double (*func)(double), double x0, double delta, const char* filename);
+
+#endif
